20428b7b886e2e42b9530aad5550384b1ca2f7db
AkkachaiCE <akachai1624@gmail.com>
2025-07-05 17:43:31 +0700

    Added args to support user prompt as argrument

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 
 def main():
     load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print('AI Assistant Usage: uv run main.py "your prompt here"')
+        sys.exit(1)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     model = 'gemini-2.0-flash-001'
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents = " ".join(args)
 
     response = client.models.generate_content(model=model, contents=contents)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"The response is:\n{response.text}")
+    
 
 if __name__ == "__main__":
 
