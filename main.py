b92986c6ef5be0ca82ae65d6f66b77b7d800736b
AkkachaiCE <akachai1624@gmail.com>
2025-07-05 18:14:20 +0700

    Added --verbose option for investigation purpose when run main.py

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ import sys
 def main():
     load_dotenv()
 
-    args = sys.argv[1:]
+    if sys.argv[-1] == "--verbose":
+        args = sys.argv[1:-1]
+        verbose = True
+    else:
+        verbose = False
+        args = sys.argv[1:]
 
     if not args:
         print('AI Assistant Usage: uv run main.py "your prompt here"')
@@ -24,12 +29,17 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
+    generate_content(model, client, messages, verbose)
 
+def generate_content(model, client, messages, verbose):
     response = client.models.generate_content(model=model, contents=messages)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"The response is:\n{response.text}")
+    if verbose:
+        print(f"User prompt: {messages[0].parts[0].text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     
+
 
 if __name__ == "__main__":
 
